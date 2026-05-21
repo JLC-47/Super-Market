af78c5a3d53a635aebdf33a11d7ca70178fcd9b7
Alejandro Alzate <alejandroalzatesena@gmail.com>
2026-05-21 09:44:57 -0500

    feat:Se inicializa el repositorio

---
+++ market/src/main/java/com/supermarket/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
