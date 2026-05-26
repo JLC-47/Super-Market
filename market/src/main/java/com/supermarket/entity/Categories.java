b6c9988584ce44643bd0528463bdeb78d7d7b321
Janier Leudo <janierleudo@gmail.com>
2026-05-26 09:25:55 -0500

    feat: Se añade algunas entidades y se corrige errores

---
+++ market/src/main/java/com/supermarket/entity/Categories.java
@@ -0,0 +1,10 @@
+package com.supermarket.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cattegories")
+public class Categories {
+    
+}
