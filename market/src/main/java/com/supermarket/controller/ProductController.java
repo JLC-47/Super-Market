46583808f9bd5ae8ba46a138675e2bc9148d7391
Janier Leudo <janierleudo@gmail.com>
2026-05-27 18:48:50 -0500

    feat: Se crea estructura inicial

--- market/src/main/java/com/supermarket/controller/ProductController.java
+++ market/src/main/java/com/supermarket/controller/ProductController.java
@@ -1,8 +1,25 @@
 package com.supermarket.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.supermarket.dto.MessageResponseDTO;
+import com.supermarket.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+    
+    private final ProductService productService;
+
+   
     
 }
