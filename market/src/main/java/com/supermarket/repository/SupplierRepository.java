407f2adf1405095088a613b425bfd388a2c9e708
Janier Leudo <janierleudo@gmail.com>
2026-05-27 22:58:37 -0500

    fix: se corrige un error

--- market/src/main/java/com/supermarket/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.supermarket.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.supermarket.entity.Suppliers;
-
-public interface SupplierRepository  extends JpaRepository<Suppliers, Long>{
-    
-}
