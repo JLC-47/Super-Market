e83a4daf07aeee9e88f3108ebfd7bd96ca64c0ef
Janier Leudo <janierleudo@gmail.com>
2026-05-27 19:45:36 -0500

    feat: Se crea estructura inicial con la funcionalidad de crear

--- market/src/main/java/com/supermarket/controller/ProductController.java
+++ market/src/main/java/com/supermarket/controller/ProductController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.supermarket.dto.MessageResponseDTO;
+import com.supermarket.dto.ProductResgisterDTO;
 import com.supermarket.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ProductController {
     
     private final ProductService productService;
+
+    @PostMapping("/create")
+    public ResponseEntity<MessageResponseDTO> createProduct(@RequestBody ProductResgisterDTO request){
+        MessageResponseDTO response = productService.createProduct(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
    
     
